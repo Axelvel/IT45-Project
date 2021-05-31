1c0f0557342055f0b57a39e4f6189ded9f36238d
Sophie Teimournia <50867904+SophieShirine@users.noreply.github.com>
2021-05-31 22:01:48 +0200

    added details

--- src/Speciality.java
+++ src/Speciality.java
@@ -1,8 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public enum Speciality {
-    MENUISERIE,
-    ELECTRICITE,
-    MECANIQUE,
-    INFORMATIQUE,
-    CUISINE
+    MENUISERIE (0),
+    ELECTRICITE (1),
+    MECANIQUE (2),
+    INFORMATIQUE (3),
+    CUISINE (4);
+
+    public final int id;
+    private static final Map<Integer, Speciality> byId = new HashMap<>();
+    static {
+        for (Speciality e: values()) {
+            byId.put(e.id, e);
+        }
+    }
+
+    Speciality(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public static Speciality valueOfId(int id) {
+        return byId.get(id);
+    }
 }
